87f73ca6443d664324225b7ec28c3c9c92e28f2a
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-21 16:38:28 +0900

    ユーザー新規登録機能

--- db/migrate/20201021050852_devise_create_users.rb
+++ db/migrate/20201021050852_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,            null: false
       t.string :email,               null: false, default: ""
       t.string :encrypted_password,  null: false, default: ""
-      t.string :fist_name,           null: false
+      t.string :frist_name,           null: false
       t.string :last_name,           null: false
       t.string :first_name_kana,     null: false
       t.string :last_name_kana,      null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_21_050852) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "fist_name", null: false
+    t.string "frist_name", null: false
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
