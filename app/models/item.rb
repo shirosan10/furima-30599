ce3ceab83b699fb8ef359e0931e58b3f516162aa
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-24 14:14:58 +0900

    Genre作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
   belongs_to :user
   has_one_attaced :image
 
@@ -10,6 +13,12 @@ class Item < ApplicationRecord
   validates :prefecture_id,   presence: true
   validates :delivery_date_id,presence: true
   validates :price,           presence: true
+
+  #空の投稿を保存できないようにする
+  validates :title, :catch_copy, :genre_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_date_id, :price, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
 
 
 end
