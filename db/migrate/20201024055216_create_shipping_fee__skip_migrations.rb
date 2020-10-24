522e2f8e963ca8efa365f20555c57c39d804b84e
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-24 15:02:11 +0900

    shipping_fee--skip-migration削除、shipping_fee作成

--- db/migrate/20201024055216_create_shipping_fee__skip_migrations.rb
+++
@@ -1,8 +0,0 @@
-class CreateShippingFeeSkipMigrations < ActiveRecord::Migration[6.0]
-  def change
-    create_table :shipping_fee__skip_migrations do |t|
-
-      t.timestamps
-    end
-  end
-end
--- spec/factories/shipping_fee__skip_migrations.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :shipping_fee_skip_migration do
-    
-  end
-end
