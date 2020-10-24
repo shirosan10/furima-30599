1b330d69504e0478b769017f21146c0f1ba19a40
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-24 15:30:41 +0900

    createアクション、ストロングパラメーター作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,24 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.valid? 
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :title, :catch_copy, :genre_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
   
 end
