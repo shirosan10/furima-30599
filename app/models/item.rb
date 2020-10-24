f90fd782120f1033485bad4813db9287dd497988
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-24 12:25:05 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  validates :title,           presence: true
+  validates :catch_copy,      presence: true
+  validates :genre_id,        presence: true
+  validates :status_id,       presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id,   presence: true
+  validates :delivery_date_id,presence: true
+  validates :price,           presence: true
+
 
 end
