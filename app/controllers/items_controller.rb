d04ebb40624c5e5399530e077e7d19c8f24adbba
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-31 16:28:26 +0900

    Revert "商品削除機能の実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :new, :create, :show]
 
   def index
@@ -34,14 +34,6 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item)
     else
       render :edit
-    end
-  end
-
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :show
     end
   end
 
