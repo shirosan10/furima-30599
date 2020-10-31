07a8be76ea6a6b50ec3048fcebb9c149ee9ca589
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-10-31 15:34:17 +0900

    updateアクション作成、非ログイン状態だとログイン画面に遷移する様設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -27,6 +29,14 @@ class ItemsController < ApplicationController
 
   end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
@@ -35,6 +45,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to : new_user_session_path
+    end
   end
 
 end
