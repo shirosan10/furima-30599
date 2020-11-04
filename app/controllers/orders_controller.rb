e80cd4f4dd1aed741ee1e1871920ab77e970bebe
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-11-04 17:06:26 +0900

    Formオブジェクトitem_order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,7 @@ class OrdersController < ApplicationController
   def create
 
   end
+
+  def index
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :item_id
+  belongs_to :user
+  belongs_to :item
   has_one :address
 end
