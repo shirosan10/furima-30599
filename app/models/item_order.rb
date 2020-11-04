35f530df68350bd04558c7d12cc1bc91c750e825
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-11-04 22:54:40 +0900

    rubocop修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,19 +1,18 @@
 class ItemOrder < ApplicationRecord
-
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :token, :postal_code, :prefecture_id, :city, :line, :building_room, :phone_number
 
   with_options presence: true do
     validates :token
-    validates :postal_code,   format: { with: /\A[0-9]{3}-[0-9]{4}\z/ } 
+    validates :postal_code,   format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :line
-    validates :phone_number,   format: {with: /\A[a-zA-Z0-9]+\z/ }
+    validates :phone_number, format: { with: /\A[a-zA-Z0-9]+\z/ }
   end
 
   def save
     order = Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,line: line, building_room: building_room,  phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, line: line, building_room: building_room, phone_number: phone_number, order_id: order.id)
   end
 end
--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -1,73 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe ItemOrder, type: :model do
-  describe "商品注文情報の保存" do
+  describe '商品注文情報の保存' do
     before do
       @Item_order = FactoryBot.build(:Item_order)
     end
-    
-    it "必要事項が全て記入されていれば購入できる" do
+
+    it '必要事項が全て記入されていれば購入できる' do
       expect(@Item_order).to be_valid
     end
 
-    it "建物名が抜けていても購入できる" do
-      @Item_order.building_room= nil
+    it '建物名が抜けていても購入できる' do
+      @Item_order.building_room = nil
       expect(@Item_order).to be_valid
     end
 
-    
-    it "カード情報が空だと購入できない" do
+    it 'カード情報が空だと購入できない' do
       @Item_order.token = nil
       @Item_order.valid?
       expect(@Item_order.errors.full_messages).to include("Token can't be blank")
     end
 
-    it "郵便番号が空だと購入できない" do
+    it '郵便番号が空だと購入できない' do
       @Item_order.postal_code = nil
       @Item_order.valid?
       expect(@Item_order.errors.full_messages).to include("Postal code can't be blank")
     end
 
-    it "郵便番号が全角数字だと購入できない" do
-      @Item_order.postal_code = "１２３４５６７"
+    it '郵便番号が全角数字だと購入できない' do
+      @Item_order.postal_code = '１２３４５６７'
       @Item_order.valid?
-      expect(@Item_order.errors.full_messages).to include("Postal code is invalid")
+      expect(@Item_order.errors.full_messages).to include('Postal code is invalid')
     end
 
-    it "郵便番号の-が抜けていると購入できない" do
-      @Item_order.postal_code = "1234567"
+    it '郵便番号の-が抜けていると購入できない' do
+      @Item_order.postal_code = '1234567'
       @Item_order.valid?
-      expect(@Item_order.errors.full_messages).to include("Postal code is invalid")
+      expect(@Item_order.errors.full_messages).to include('Postal code is invalid')
     end
 
-    it "都道府県が--だと購入できない" do
-      @Item_order.prefecture_id = "---"
+    it '都道府県が--だと購入できない' do
+      @Item_order.prefecture_id = '---'
       @Item_order.valid?
-      expect(@Item_order.errors.full_messages).to include("Prefecture is not a number")
+      expect(@Item_order.errors.full_messages).to include('Prefecture is not a number')
     end
 
-    it "市区町村が空だと購入できない" do
+    it '市区町村が空だと購入できない' do
       @Item_order.city = nil
       @Item_order.valid?
       expect(@Item_order.errors.full_messages).to include("City can't be blank")
     end
 
-    it "番地が空だと購入できない" do
+    it '番地が空だと購入できない' do
       @Item_order.line = nil
       @Item_order.valid?
       expect(@Item_order.errors.full_messages).to include("Line can't be blank")
     end
 
-    it "電話番号が空だと購入できない" do
+    it '電話番号が空だと購入できない' do
       @Item_order.phone_number = nil
       @Item_order.valid?
       expect(@Item_order.errors.full_messages).to include("Phone number can't be blank")
     end
 
-    it "電話番号が全角数字だと購入できない" do
-      @Item_order.phone_number = "０7０１２３４５６７８"
+    it '電話番号が全角数字だと購入できない' do
+      @Item_order.phone_number = '０7０１２３４５６７８'
       @Item_order.valid?
-      expect(@Item_order.errors.full_messages).to include("Phone number is invalid")
+      expect(@Item_order.errors.full_messages).to include('Phone number is invalid')
     end
   end
 end
