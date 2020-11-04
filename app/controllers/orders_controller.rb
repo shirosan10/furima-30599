c4e141a08ccd056b762fe05553f94fce8d8e7c10
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-11-04 17:23:57 +0900

    orderコントローラーview編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,26 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def index
+    @item_order = ItemOrder.new
+  end
+
   def create
 
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
-  def index
+  private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :line, :building_room, :phone_number).merge(token: params[:token])
   end
+
+  
+
 end
