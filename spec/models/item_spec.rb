d8ad7277fef95be39684105ddfb40a7250973d56
shirosan10 <71954946+shirosan10@users.noreply.github.com>
2020-11-15 17:57:47 +0900

    単体テストコードを日本語に書き換え

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,57 +17,57 @@ RSpec.describe Item, type: :model do
       it '商品名が空だと登録できない' do
         @item.title = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Title can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it '商品の説明がないと登録できない' do
         @item.catch_copy = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Catch copy can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       it 'カテゴリーの情報がないと登録できない' do
         @item.category_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category Select')
+        expect(@item.errors.full_messages).to include('カテゴリーを入力してください')
       end
       it '商品の状態についての情報がないと登録できない' do
         @item.status_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('Status Select')
+        expect(@item.errors.full_messages).to include('商品の状態を入力してください')
       end
       it '配送料の負担についての情報がないと登録できない' do
         @item.shipping_fee_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping fee Select')
+        expect(@item.errors.full_messages).to include('配送料を入力してください')
       end
       it '発送元の地域についての情報がないと登録できない' do
         @item.prefecture_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture Select')
+        expect(@item.errors.full_messages).to include('発送元の地域を入力してください')
       end
       it '発送までの日数についての情報がないと登録できない' do
         @item.delivery_date_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery date Select')
+        expect(@item.errors.full_messages).to include('発送までの日数を入力してください')
       end
       it '価格についての情報がないと登録できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("価格を入力してください")
       end
       it '価格が¥300以下だと登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is out of setting range')
+        expect(@item.errors.full_messages).to include('価格が¥300以下だと登録できません')
       end
       it '金額が10,000,000円以上だと登録できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is out of setting range')
+        expect(@item.errors.full_messages).to include('金額が10,000,000円以上だと登録できません')
       end
       it '販売価格が全角数字だと登録できない' do
         @item.price = '１２３４５'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is out of setting range')
+        expect(@item.errors.full_messages).to include('販売価格が全角数字だと登録できません')
       end
     end
   end
